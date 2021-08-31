a5a353867f4917fcca170942dd975018aeaf89c4
Gábor Tamás Tornai <gabo0127@edu.easj.dk>
2021-08-31 15:04:41 +0200

    practice

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        private int _x;
+        private int _y;
+
+        public Demo(int x, int y)
+        {
+            x = _x;
+            y = _y;
+
+        }
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public  int Y { get; set; }
+
+    }
+}
